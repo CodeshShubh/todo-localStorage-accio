50db9b67345e2a6f2e5e41ad925c8369cb9cdecf
CodeshShubh <codeshshubh@gmail.com>
2025-02-02 06:40:19 +0530

    future Task with priority sort

--- inex.js
+++ inex.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-    let tasksArr = JSON.parse(localStorage.getItem('Tasks'))||[]
-    console.log('global task arr',tasksArr);
+    let todayTasks = JSON.parse(localStorage.getItem('todayTasks'))||[]
+    let futureTasks = JSON.parse(localStorage.getItem('futureTasks'))||[]
+    console.log('global task arr',todayTasks);
 
 function getTaskInputs(){
 return{
      taskName: document.getElementById('task_name').value.trim(),
-     selctedDate: document.getElementById('selected_date').value,
+     selectedDate: document.getElementById('selected_date').value,
      selectedOption: document.getElementById('selected_option').value,
 }
 
@@ -13,28 +14,63 @@ return{
 let addBtn = document.getElementById('add_btn');
 
 
-addBtn.addEventListener('click', todayTask);
+addBtn.addEventListener('click', AddTask);
 
-function todayTask(){
+function AddTask(){
     
-      let {taskName, selctedDate , selectedOption} = getTaskInputs();
-    // console.log(taskName, selctedDate , selectedOption);
-    if(taskName=='' || selctedDate=='' || selectedOption=='Priority'){
+      let {taskName, selectedDate , selectedOption} = getTaskInputs();
+    // console.log(taskName, selectedDate , selectedOption);
+    if(taskName=='' || selectedDate=='' || selectedOption=='Priority'){
         alert('Please Enter Task first with all Details  😡')
-    }else if(new Date(selctedDate) < (new Date())){
+    }else if(new Date(selectedDate).setHours(0,0,0,0) < new Date().setHours(0,0,0,0)){
         alert('you can not Enter Past Date 😡')
-    }
-    else{
+
+    }else if(new Date(selectedDate).setHours(0,0,0,0) === new Date().setHours(0,0,0,0)){
+        // alert('you Task is added 😡')
         let task = {
             taskName: taskName,
-            selctedDate: selctedDate,
+            selectedDate: selectedDate,
             selectedOption: selectedOption
          }
-         tasksArr.push(task);
-         localStorage.setItem('todayTasks', JSON.stringify(tasksArr))
+         todayTasks.push(task);
+         PushOnPriorityBasis(todayTasks , 'todayTasks') // sending parameter to the function
+        //  localStorage.setItem('todayTasks', JSON.stringify(todayTasks))
+
+    }else if(new Date(selectedDate).setHours(0,0,0,0) > new Date().setHours(0,0,0,0)){
+
+        let task = {
+            taskName: taskName,
+            selectedDate: selectedDate,
+            selectedOption: selectedOption
+         }
+         futureTasks.push(task);
+         PushOnPriorityBasis(futureTasks , 'futureTasks') // sending parameter to the function
+        //  localStorage.setItem('futureTasks', JSON.stringify(futureTasks))
+             
     }
-    renderTodayTasks()      
+    renderTodayTasks();
+    renderFutureTasks();   
 }
+
+// this is for store tasks on prority basis 
+
+
+function PushOnPriorityBasis(tasks , key){
+
+    const prority = {
+        High:1,
+        Medium:2,
+        Low:3,
+    };
+
+    tasks.sort((a,b)=> prority[a.selectedOption] - prority[b.selectedOption]);
+
+    localStorage.setItem(key, JSON.stringify(tasks));
+
+}
+
+
+
 
  // setting today date object
 function nowDate(){
@@ -49,16 +85,14 @@ function nowDate(){
 
 function renderTodayTasks(){
     let {today} = nowDate();
-
-//   console.log(tasksArr[0].selctedDate);
   let todayList = document.getElementById('today_list')
   todayList.innerHTML = ``
-  tasksArr.forEach((task, index)=>{
-    if(task.selctedDate == today){
+  todayTasks.forEach((task, index)=>{
+    if(task.selectedDate == today){
        todayList.innerHTML +=`
           <div class="list_card">
                 <p>${index +1}. ${task.taskName}</p>
-                <p>${task.selctedDate}</p>
+                <p>${task.selectedDate}</p>
                 <p>Priority: ${task.selectedOption}</p>
                 <div class="delete_check_icons">
                     <img src="./icons8-check.gif" alt="check"/>
@@ -72,6 +106,34 @@ function renderTodayTasks(){
   
 }
 renderTodayTasks();
+
+
+
+
+
+function renderFutureTasks(){
+    // let {today} = nowDate();
+  let futureList = document.getElementById('future_list')
+  futureList.innerHTML = ``
+  futureTasks.forEach((task, index)=>{
+    // if(task.selectedDate == today){
+        futureList.innerHTML +=`
+          <div class="list_card">
+                <p>${index +1}. ${task.taskName}</p>
+                <p>${task.selectedDate}</p>
+                <p>Priority: ${task.selectedOption}</p>
+                <div class="delete_check_icons">
+                    <img src="./icons8-check.gif" alt="check"/>
+                    <img src="./icons8-delete.gif" alt="delete"/>
+                </div>
+            </div>
+       `
+    // }
+     
+  })
+  
+}
+renderFutureTasks();
 
 
 
